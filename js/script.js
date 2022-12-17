4045922584bb6188d0836103434786f93c30c117
Samuel de Carvalho <samuel.xw12@gmail.com>
2022-12-16 21:34:00 -0300

    Validações

--- js/script.js
+++ js/script.js
@@ -9,6 +9,10 @@ let btnCriar = document.getElementById('btncriar')
 
 let divCards = document.getElementById('cards')
 
+let animeAlert = document.getElementById('animealert')
+let personagemAlert = document.getElementById('personagemalert')
+let imgAlert = document.getElementById('imgalert')
+
 btnCriar.addEventListener('click', (ev) => {
     ev.preventDefault()
     // Criando elementos a serem inseridos na div cards
@@ -18,6 +22,8 @@ btnCriar.addEventListener('click', (ev) => {
     let h3Personagem = document.createElement('h3')
     let imgPersonagem = document.createElement('img')
     imgPersonagem.setAttribute('class', 'imgcard')
+    imgPersonagem.setAttribute('alt', 'Seu link de imagem é inválido!')
+    imgPersonagem.style.color = '#fff'
 
     //  Adicionando valores aos elementos
     h2Anime.innerText = animeInput.value 
@@ -32,4 +38,55 @@ btnCriar.addEventListener('click', (ev) => {
     // Adicionando o novo card a div cards
     divCards.appendChild(novoCard)
 })
+
+// Validções
+// input anime
+let animeValido;
+animeInput.addEventListener('keyup', () =>{
+    if(animeInput.value.length < 4){
+        animeAlert.innerText = 'Min: 4 caracteres'
+        animeValido = false
+    }else {
+        animeValido = true
+        animeAlert.innerText = ''
+    }
+})
+
+//input personagem
+let personagemValido;
+personagemInput.addEventListener('keyup', () =>{
+    if(personagemInput.value.length < 4){
+        personagemAlert.innerText = 'Min: 4 caracteres'
+        personagemValido = false
+    }else {
+        personagemValido = true
+        personagemAlert.innerText = ''
+    }
+})
+
+// input img
+let imgValido;
+imgInput.addEventListener('keyup', () =>{
+    if(imgInput.value.length <= 0){
+        imgAlert.innerText = 'Digite o link de uma imagem!'
+        imgValido = false;
+    } else {
+        imgValido = true;
+        imgAlert.innerText = ''
+    }
+})
+
+document.addEventListener('keyup', () =>{
+    if(animeValido == true && personagemValido == true && imgValido == true){
+        btnCriar.removeAttribute('disabled')
+        btnCriar.style.backgroundColor = '#fff'
+    } else {
+        btnCriar.setAttribute('disabled', true)
+        btnCriar.style.backgroundColor = '#fff4'
+    }
+})
+
+
+
+
 
