06f24ccf8b2766965d579bcbacd635ad9d06629c
Samuel de Carvalho <samuel.xw12@gmail.com>
2022-12-18 20:48:10 -0300

    "Finalizado"

--- js/script.js
+++ js/script.js
@@ -13,32 +13,6 @@ let animeAlert = document.getElementById('animealert')
 let personagemAlert = document.getElementById('personagemalert')
 let imgAlert = document.getElementById('imgalert')
 
-btnCriar.addEventListener('click', (ev) => {
-    ev.preventDefault()
-    // Criando elementos a serem inseridos na div cards
-    let novoCard = document.createElement('div')
-    novoCard.setAttribute('class', 'card')
-    let h2Anime = document.createElement('h2')
-    let h3Personagem = document.createElement('h3')
-    let imgPersonagem = document.createElement('img')
-    imgPersonagem.setAttribute('class', 'imgcard')
-    imgPersonagem.setAttribute('alt', 'Seu link de imagem é inválido!')
-    imgPersonagem.style.color = '#fff'
-
-    //  Adicionando valores aos elementos
-    h2Anime.innerText = animeInput.value 
-    h3Personagem.innerText = personagemInput.value
-    imgPersonagem.setAttribute('src', imgInput.value)
-
-    // Adicionando elementos ao novo card
-    novoCard.appendChild(h2Anime)
-    novoCard.appendChild(h3Personagem)
-    novoCard.appendChild(imgPersonagem)
-
-    // Adicionando o novo card a div cards
-    divCards.appendChild(novoCard)
-})
-
 // Validções
 // input anime
 let animeValido;
@@ -68,25 +42,76 @@ personagemInput.addEventListener('keyup', () =>{
 let imgValido;
 imgInput.addEventListener('keyup', () =>{
     if(imgInput.value.length <= 0){
-        imgAlert.innerText = 'Digite o link de uma imagem!'
         imgValido = false;
     } else {
         imgValido = true;
-        imgAlert.innerText = ''
     }
 })
 
+document.body.addEventListener('mousemove', () =>{
+    if(imgInput.value.length <= 0){
+        imgValido = false;
+    } else {
+        imgValido = true;
+    }
+})
+
+// Habilitando o btn
 document.addEventListener('keyup', () =>{
     if(animeValido == true && personagemValido == true && imgValido == true){
         btnCriar.removeAttribute('disabled')
         btnCriar.style.backgroundColor = '#fff'
+        imgAlert.innerText = ''
     } else {
         btnCriar.setAttribute('disabled', true)
         btnCriar.style.backgroundColor = '#fff4'
+        imgAlert.innerText = 'Preencha todos os campos!'
+    }
+})
+
+document.addEventListener('mousemove', () =>{
+    if(animeValido == true && personagemValido == true && imgValido == true){
+        btnCriar.removeAttribute('disabled')
+        btnCriar.style.backgroundColor = '#fff'
+        imgAlert.innerText = ''
+    } else {
+        btnCriar.setAttribute('disabled', true)
+        btnCriar.style.backgroundColor = '#fff4'
+        imgAlert.innerText = 'Preencha todos os campos!'
     }
 })
 
 
+btnCriar.addEventListener('click', (ev) => {
+    ev.preventDefault()
+    // Criando elementos a serem inseridos na div cards
+    let novoCard = document.createElement('div')
+    novoCard.setAttribute('class', 'card')
+    let h2Anime = document.createElement('h2')
+    let h3Personagem = document.createElement('h3')
+    let imgPersonagem = document.createElement('img')
+    imgPersonagem.setAttribute('class', 'imgcard')
+    imgPersonagem.setAttribute('alt', 'Seu link de imagem é inválido!')
+    imgPersonagem.style.color = '#fff'
+
+    //  Adicionando valores aos elementos
+    h2Anime.innerText = animeInput.value 
+    h3Personagem.innerText = personagemInput.value
+    imgPersonagem.setAttribute('src', imgInput.value)
+
+    // Adicionando elementos ao novo card
+    novoCard.appendChild(h2Anime)
+    novoCard.appendChild(h3Personagem)
+    novoCard.appendChild(imgPersonagem)
+
+    // Adicionando o novo card a div cards
+    divCards.appendChild(novoCard)
+
+    // Limpando o form
+    animeInput.value = ''
+    personagemInput.value = ''
+    imgInput.value = ''
+})
 
 
 
